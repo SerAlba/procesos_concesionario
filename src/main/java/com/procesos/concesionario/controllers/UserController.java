a39967d042316d0941487114dc31fb30efacfa17
SCIS3-PC17\SCIS3-17 <correo>
2023-03-23 20:52:58 -0500

    Version 1.0.2 - CRUD completado

--- src/main/java/com/procesos/concesionario/controllers/UserController.java
+++ src/main/java/com/procesos/concesionario/controllers/UserController.java
@@ -3,7 +3,12 @@ package com.procesos.concesionario.controllers;
 import com.procesos.concesionario.models.User;
 import com.procesos.concesionario.services.UserServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -11,12 +16,70 @@ public class UserController {
     private UserServiceImp userServiceImp;
 
     @GetMapping(value = "/user/{id}")
-    public User getById (@PathVariable(name = "id") Long id) {
-        return userServiceImp.getUserById(id);
+    public ResponseEntity getById (@PathVariable(name = "id") Long id) {
+        Map response = new HashMap();
+
+        try {
+            response.put("message", "User found");
+            response.put("data", userServiceImp.getUserById(id));
+
+            return new ResponseEntity(response, HttpStatus.OK);
+        } catch (Exception e){
+            response.put("message", "User not found");
+            response.put("data", e.getMessage());
+
+            return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PostMapping(value = "/user")
-    public User createUser (@RequestBody User user) {
-        return userServiceImp.createUser(user);
+    public ResponseEntity createUser (@RequestBody User user) {
+        Map response = new HashMap();
+
+        try {
+            response.put("message", "User created");
+            response.put("data", userServiceImp.createUser(user));
+
+            return new ResponseEntity(response, HttpStatus.CREATED);
+        } catch (Exception e){
+            response.put("message", "Error creating user");
+            response.put("data", e.getMessage());
+
+            return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping(value = "/listUsers")
+    public ResponseEntity getListUsers () {
+        Map response = new HashMap();
+
+        try {
+            response.put("message", "Users found");
+            response.put("data", userServiceImp.allUsers());
+
+            return new ResponseEntity(response, HttpStatus.OK);
+        } catch (Exception e){
+            response.put("message", "There are no users");
+            response.put("data", e.getMessage());
+
+            return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping(value = "/user/{id}")
+    public ResponseEntity updateUser (@PathVariable(name = "id") Long id, @RequestBody User user) {
+        Map response = new HashMap();
+
+        try {
+            response.put("message", "Updated user");
+            response.put("data", userServiceImp.updateUser(id, user));
+
+            return new ResponseEntity(response, HttpStatus.OK);
+        } catch (Exception e){
+            response.put("message", "Error updating user");
+            response.put("data", e.getMessage());
+
+            return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        }
     }
 }
